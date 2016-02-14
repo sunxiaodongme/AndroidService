eb3932e797d47f9c15a3802a88718b0084f3e9e1
sunxiaodongme <hys_521@126.com>
2016-02-14 11:45:43 +0800

    远程服务使用示例
    远程服务使用示例

---
+++ RemoteService/app/src/main/aidl/com/example/sunxiaodong/remoteservice/IMyServiceAIDL.aidl
@@ -0,0 +1,10 @@
+// IMyServiceAIDL.aidl
+package com.example.sunxiaodong.remoteservice;
+
+// Declare any non-default types here with import statements
+
+interface IMyServiceAIDL {
+
+    int add(int a, int b);
+
+}
---
+++ RemoteService/app/src/main/java/com/example/sunxiaodong/remoteservice/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.sunxiaodong.remoteservice;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String NAME = MainActivity.class.getSimpleName();
+    private static final String TAG = "sxd";
+
+    private Button mStartService;
+    private Button mStopService;
+    private Button mBindService;
+    private Button mUnbindService;
+
+    private EditText mAddendOne;
+    private EditText mAddendTwo;
+    private TextView mResult;
+
+    private IMyServiceAIDL mIMyServiceAIDL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mStartService = (Button) this.findViewById(R.id.start_service);
+        mStartService.setOnClickListener(this);
+        mStopService = (Button) this.findViewById(R.id.stop_service);
+        mStopService.setOnClickListener(this);
+        mBindService = (Button) this.findViewById(R.id.bind_service);
+        mBindService.setOnClickListener(this);
+        mUnbindService = (Button) this.findViewById(R.id.unbind_service);
+        mUnbindService.setOnClickListener(this);
+        mAddendOne = (EditText) this.findViewById(R.id.addend_one);
+        mAddendTwo = (EditText) this.findViewById(R.id.addend_two);
+        mResult = (TextView) this.findViewById(R.id.result);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                startService();
+                break;
+            case R.id.stop_service:
+                stopService();
+                break;
+            case R.id.bind_service:
+                bindService();
+                break;
+            case R.id.unbind_service:
+                unbindService();
+                break;
+        }
+    }
+
+    private void startService() {
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
+    }
+
+    private void stopService() {
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
+    }
+
+    private void bindService() {
+        Intent intent = new Intent(this, MyService.class);//显示启动Service
+        /*Intent intent = new Intent("com.example.sunxiaodong.remoteservice.MyService");
+        intent.setPackage("com.example.sunxiaodong.remoteservice");//Android5.0后Service不能采用隐式启动，所以必须加上包名*/
+        bindService(intent, mConnection, BIND_AUTO_CREATE);
+    }
+
+    private void unbindService() {
+        unbindService(mConnection);
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mIMyServiceAIDL = IMyServiceAIDL.Stub.asInterface(service);
+            try {
+                int result = mIMyServiceAIDL.add(Integer.parseInt(mAddendOne.getText().toString()), Integer.parseInt(mAddendTwo.getText().toString()));
+                mResult.setText(String.valueOf(result));
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+}
